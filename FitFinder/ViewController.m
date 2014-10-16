6495ecae330ace9511534fb3ec35b8b02271eeeb
Michael Brennan <michael_rb@utexas.edu>
2014-10-15 20:55:56 -0500

    Took out the test object

--- FitFinder/ViewController.m
+++ FitFinder/ViewController.m
@@ -17,11 +17,6 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-    
-    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
-    testObject[@"test"] = @"some test thang";
-    [testObject saveInBackground];
     
 }
 
