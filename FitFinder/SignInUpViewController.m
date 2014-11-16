23bf388b38634678375da1b78d0fbac1e242046a
baibye <carlbai@gmail.com>
2014-11-15 22:33:19 -0600

    Image added to collectionview, not aligned/centered

--- FitFinder/SignInUpViewController.m
+++ FitFinder/SignInUpViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SignInUpViewController.h"
+#import "PhotoCell.h"
 
 @interface SignInUpViewController ()
 
@@ -21,6 +22,13 @@
 @property UIButton *runningButton;
 @property Workout workoutType;
 @property UITextField *fullName;
+
+
+//picture stuff
+@property (strong, nonatomic) NSArray* photoFileNameArray;
+@property (strong, nonatomic) NSString* sourcePath;
+
+
 @end
 
 @implementation SignInUpViewController
@@ -49,22 +57,45 @@
     [_collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:@"cellIdentifier"];
     [self.view addSubview:_collectionView];
     
-    
+    [self loadPictures];
     [super viewDidLoad];
 
 }
 
+
+- (void) loadPictures
+{
+    // Save our path to the photos.
+    self.sourcePath = [[[NSBundle mainBundle] resourcePath] stringByAppendingPathComponent:@"Pictures"];
+    // Save photo names in photoArray.
+    self.photoFileNameArray = [[NSFileManager defaultManager] contentsOfDirectoryAtPath:self.sourcePath error:NULL];
+}
+
 - (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section
 {
-    return 3;
+    return [self.photoFileNameArray count];
+    //return 3;
 }
 
 // The cell that is returned must be retrieved from a call to -dequeueReusableCellWithReuseIdentifier:forIndexPath:
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath
 {
+    // Get image name and create a file extension for it.
+    NSString* imageName = [self.photoFileNameArray objectAtIndex:indexPath.row];
+    NSString *filename = [NSString stringWithFormat:@"%@/%@", self.sourcePath, imageName];
+    
+    
     UICollectionViewCell *cell=[collectionView dequeueReusableCellWithReuseIdentifier:@"cellIdentifier" forIndexPath:indexPath];
     
     cell.backgroundColor=[UIColor whiteColor];
+    
+    // Put our images into image views.
+    UIImage* image = [UIImage imageWithContentsOfFile:filename];
+    UIImageView* photoImageView = [[UIImageView alloc] initWithImage:image];
+    
+    // Add picture to cell as subview
+    [cell addSubview:photoImageView];
+    
     return cell;
 }
 
@@ -72,6 +103,7 @@
 {
     return CGSizeMake(self.view.frame.size.width, 250);
 }
+
 
 - (BOOL)textFieldShouldReturn:(UITextField *)textField
 {
